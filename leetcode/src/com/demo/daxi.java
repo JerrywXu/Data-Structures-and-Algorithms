234d0fa9a16a25cb40803609896be9034238a47c
“JerrywXu” <jerryy00@163.com>
2021-02-04 20:42:50 +0800

    update

--- leetcode/src/com/demo/daxi.java
+++ leetcode/src/com/demo/daxi.java
@@ -1,5 +1,24 @@
 package com.demo;
 
 public class daxi {
-    
+    public static void main(String[] args) {
+
+    }
+
+    public static int superDigit(String n,int k){
+
+        int j=Integer.parseInt(n);
+        int sum=fun(j)*k;
+
+        return sum;
+    }
+
+    public static int fun(int n){
+        int ret = 0;
+        if(n<10){
+            return n;
+        }
+        return n%10 + fun(n/10);
+    }
+
 }
--- leetcode/src/com/interview/aiqiyiduoshuyuansu.java
+++
@@ -1,7 +0,0 @@
-package com.interview;
-
-/**
- * Created by 徐纪伟 on 2020/9/13.
- */
-public class aiqiyiduoshuyuansu {
-}
