e678b1e954b7cfba2d73cd2a3258b630afa1b094
“JerrywXu” <jerryy00@163.com>
2021-05-01 19:17:38 +0800

    update

---
+++ leetcode/src/com/code/demo58_2.java
@@ -0,0 +1,19 @@
+package com.code;
+
+import java.util.Arrays;
+
+public class demo58_2 {
+    public static void main(String[] args) {
+
+    }
+    public int tdepth (int[] arr) {
+        // write code here
+        //Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            
+        }
+
+
+
+    }
+}
